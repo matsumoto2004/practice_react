cdc54e1ea4d79bf2283c874740326cef9293f844
matsumoto2004 <mamamaturi57@gmail.com>
2024-11-28 22:25:14 +0900

    #13 finished

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,11 +1,11 @@
-const BlogList = ({blogs}) => {
-  //const blogs=props.blogs
+const BlogList = ({blogs,title,handleDelete}) => {
   return ( 
     <div className="bloglist">
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
+          <button onClick={() => handleDelete(blog.id)}>delete blog</button>
         </div>
       ))}
     </div>
