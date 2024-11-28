b3ca6b1484e8c1a8ed7cc839e3c098fe449615c1
松本凜太郎 <matsumotorintaro@matsumotorintarounoMacBook-Air.local>
2024-11-28 21:10:38 +0900

    #10 finished

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,21 @@
 import {useState} from 'react';
 
 const Home = () => {
-  //let name='mario';
-  const [name,setName]=useState('mario');
-  const [age,setAge]=useState(25)
-  const handleClick = () => {
-    setName('luigi');
-    setAge(30);
-  }
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
   
-
+  
   return ( 
     <div className="home">
-      <h2>Homepage</h2>
-      <button onClick={handleClick}>Click me</button>
-      <p>{name} is {age} years old</p>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 }
