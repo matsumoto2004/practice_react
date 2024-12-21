49979fed2fb5a802913bf84a413717c8299b9b03
matsumoto2004 <mamamaturi57@gmail.com>
2024-12-22 01:21:27 +0900

    #20 finished,react-dom installed

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,11 +1,10 @@
-const BlogList = ({blogs,title,handleDelete}) => {
+const BlogList = ({blogs,title}) => {
   return ( 
     <div className="bloglist">
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
-          <button onClick={() => handleDelete(blog.id)}>delete blog</button>
         </div>
       ))}
     </div>
