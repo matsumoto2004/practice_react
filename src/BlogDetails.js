f7361cc8c5317e9bbadff2f57dccf6a109b0bfe2
matsumoto2004 <mamamaturi57@gmail.com>
2024-12-22 17:12:10 +0900

    #32 finished

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,20 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import { useNavigate } from 'react-router-dom';
 
 const BlogDetails = () => {
   const {id} = useParams();
   const {data:blog , isPending, error} = useFetch('http://localhost:8000/blogs/' + id);
+  const navigate = useNavigate(); 
+
+  const handleClick = () => {
+    fetch('http://localhost:8000/blogs/' + blog.id, {
+      method: 'DELETE'
+    }).then(() => {
+      navigate("/");
+
+    })
+  }
   
   return ( 
     <div className="blog-details">
@@ -14,6 +25,7 @@ const BlogDetails = () => {
           <h2>Blog Details - {id}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleClick}>Delete</button>
         </article>
       )}
     </div>
--- src/Create.js
+++ src/Create.js
@@ -1,14 +1,34 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Create = () => {
   const[title, setTitle] = useState('');
   const[body, setBody] = useState('');
   const[author, setAuthor] = useState('mario');
+  const[isPending, setIsPending] = useState(false);
+  const navigate = useNavigate();
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    setIsPending(true);
+
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog)
+    }).then(() => {
+      console.log('new blog added');
+      navigate("/");
+    })
+
+    setIsPending(false);
+  }
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog Title:</label>
         <input
           type="text"
@@ -30,7 +50,8 @@ const Create = () => {
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add Blog</button>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disabled>Adding Blog...</button>}
       </form>
     </div>
   );
